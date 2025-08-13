1b1cc3822261ba6c24bdf4155e7800e505e481b9
azrilpramudia <azrilpramudia01@gmail.com>
2025-08-13 21:31:07 +0700

    add controller

---
+++ src/controllers/PostController.ts
@@ -0,0 +1,14 @@
+import prisma from "../../prisma/client";
+
+export async function getPosts() {
+    try{
+        const posts = await prisma.post.findMany({ orderBy: {id: "desc" } });
+        return {
+            success: true,
+            message: "List Data Posts!",
+            data: posts,
+        };
+    } catch (e: unknown) {
+        console.error(`Error Getting Posts: ${e}`);
+    }
+}
